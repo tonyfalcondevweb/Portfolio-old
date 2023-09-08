c155f531a431f18553ec59e3a3582e2c745208d9
tony <tonyfalcondevweb@gmail.com>
2023-09-09 01:54:16 +0200

    ajout font et nav anime

--- myjs.js
+++ myjs.js
@@ -1,7 +1,16 @@
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
 
-    var header = document.querySelector("header");
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("nav-scroll", window.scrollY > 0);
+    nav.classList.toggle("nav-reset", window.scrollY == 0);
 
-    header.classList.toggle("sticky", window.scrollY > 0);
+    var name = this.document.getElementById("nom");
+    name.classList.toggle("name-scroll", window.scrollY > 0);
+    name.classList.toggle("name-reset", window.scrollY == 0);
 
-})
+    
+    var menu = this.document.getElementById("menu");
+    menu.classList.toggle("menu-scroll", window.scrollY > 0);
+    menu.classList.toggle("menu-reset", window.scrollY == 0);
+
+});
