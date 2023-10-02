38d9390d7de515fcfcdc9d841dd7d243f3ec26c8
tony <tonyfalcondevweb@gmail.com>
2023-10-02 17:22:08 +0200

    maj

--- myjs.js
+++ myjs.js
@@ -12,5 +12,4 @@ window.addEventListener("scroll", function () {
     var menu = this.document.getElementById("menu");
     menu.classList.toggle("menu-scroll", window.scrollY > 0);
     menu.classList.toggle("menu-reset", window.scrollY == 0);
-
 });
