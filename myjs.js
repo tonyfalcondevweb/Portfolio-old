bc65409dca6c4c3b95a1a5cebde81a0757bfc640
tony <tonyfalcondevweb@gmail.com>
2023-10-03 18:02:24 +0200

    refont visuel et animation navbar

--- myjs.js
+++ myjs.js
@@ -1,14 +1,4 @@
-window.addEventListener("scroll", function () {
-
-    var nav = document.querySelector("nav");
-    nav.classList.toggle("nav-scroll", window.scrollY > 0);
-    nav.classList.toggle("nav-reset", window.scrollY == 0);
-
-    var name = this.document.getElementById("nom");
-    name.classList.toggle("name-scroll", window.scrollY > 0);
-    name.classList.toggle("name-reset", window.scrollY == 0);
-
-    
+window.addEventListener("scroll", function () {   
     var menu = this.document.getElementById("menu");
     menu.classList.toggle("menu-scroll", window.scrollY > 0);
     menu.classList.toggle("menu-reset", window.scrollY == 0);
