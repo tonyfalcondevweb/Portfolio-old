1b8da37a8a241112f93546b84e14e897ecaa86fd
tony <tonyfalcondevweb@gmail.com>
2023-10-04 17:16:09 +0200

    presentation mag

--- myjs.js
+++ myjs.js
@@ -3,3 +3,21 @@ window.addEventListener("scroll", function () {
     menu.classList.toggle("menu-scroll", window.scrollY > 0);
     menu.classList.toggle("menu-reset", window.scrollY == 0);
 });
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add("slide-right-show");
+            entry.target.classList.add("slide-left-show");
+        }
+        else{
+            entry.target.classList.remove("slide-right-show");
+            entry.target.classList.remove("slide-left-show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.animation');
+hiddenElements.forEach((el) => observer.observe(el));
